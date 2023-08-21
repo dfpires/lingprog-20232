22efc1bd33057abf53908de06ca932659486d746
Daniel Pires <dfpires@gmail.com>
2023-08-21 08:11:53 -0300

    Add files via upload

---
+++ lista1/lista1.js
@@ -0,0 +1,16 @@
+function exe1(){
+    // recupera os dados do html
+    let nro1 = document.getElementById("nro1").value
+    let nro2 = document.getElementById("nro2").value
+    // calcula a subtração
+    let subtracao = nro1 - nro2
+    // envia o resultado para o html
+    document.getElementById("subtracao").innerHTML = 
+        `A subtração é ${subtracao}`
+}
+function exe2(){
+
+}
+function exe3(){
+
+}
