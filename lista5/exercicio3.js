078d1210cc1ff146290bbb23b6defc106f3129f9
Daniel Pires <dfpires@gmail.com>
2023-10-09 08:56:27 -0300

    Create exercicio3.js

---
+++ lista5/exercicio3.js
@@ -0,0 +1,11 @@
+Faça um programa em JS que guarde em um vetor dados de 5  partidos políticos: código, nome, sigla, presidente, nro de políticos
+
+Em seguida, guarde em um vetor dados de 5 políticos: código do partido, nome do político, qtde de votos na eleição atual (inicie com 0) e cargo que ocupa
+
+ Não é possível crias dois partidos com o mesmo código
+
+ Não é possível cadastrar um político de um partido que não exista
+
+Ao final, inicie uma votação, faça o cadastro de 10 votos, e atualize a qtde de votos do político. Não é possível votar em candidato que não exista, votar em um candidato de um partido errado.
+
+Finalmente, mostrar os dados do político que teve mais votos. 
