cf7fbea9853dd29afa7ea398b8f275cd6385b6e7
Daniel Pires <dfpires@gmail.com>
2023-10-31 10:01:08 -0300

    Add files via upload

--- lista7/exe4.js
+++ lista7/exe4.js
@@ -15,20 +15,30 @@ function calculaSituacao(vetor, matriz){
             soma = soma  + matriz[i][j]
         }
         if (soma/5 >= 6){
-            console.log(`${vetor[i]} foi aprovado com média ${soma/5}`)
+            console.log(`${vetor[i]} foi aprovado com média ${(soma/5).toFixed(1)}`)
         }
         else if (soma/5 >= 3){
-            console.log(`${vetor[i]} ficou de exame com média ${soma/5}`)
+            console.log(`${vetor[i]} ficou de exame com média ${(soma/5).toFixed(1)}`)
         }
         else {
-            console.log(`${vetor[i]} foi reprovado com média ${soma/5}`)
+            console.log(`${vetor[i]} foi reprovado com média ${(soma/5).toFixed(1)}`)
         } 
     }
 }
-
+function calculaMediaFinal(matriz){
+    let soma = 0
+    for(let i=0;i<15;i++){
+        for(let j=0;j<5;j++){
+            soma = soma + matriz[i][j]
+        }
+    }
+    console.log(`A média final da turma é ${(soma/75).toFixed(1)}`)
+}
 function principal(){
     let vetor = []
     let matriz = []
     lerDados(vetor, matriz)
     calculaSituacao(vetor, matriz)
+    calculaMediaFinal(matriz)
 }
+principal()
